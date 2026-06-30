0820a8b34f6d2843d94e006233fc08f8a5eb880a
ThanhQuang1810 <nguyentranthanhquang1810@gmail.com>
2026-06-30 22:11:12 +0700

    update structure

--- Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/repository/HackathonRoundRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seal.hackathon.repository;
-
-import com.seal.hackathon.entity.HackathonRound;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-
-public interface HackathonRoundRepository extends JpaRepository<HackathonRound, Integer> {
-    List<HackathonRound> findByEventIdOrderByRoundOrderAsc(Integer eventId);
-}
