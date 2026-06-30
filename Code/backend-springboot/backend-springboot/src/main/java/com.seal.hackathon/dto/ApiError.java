31cd9b0761c58542b05b7e121a5ea0a8de2a1fc0
ThanhQuang1810 <nguyentranthanhquang1810@gmail.com>
2026-06-30 22:11:12 +0700

    update structure

--- Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/dto/ApiError.java
+++
@@ -1,9 +0,0 @@
-package com.seal.hackathon.dto;
-
-import java.time.LocalDateTime;
-
-public record ApiError(String errorCode, String message, LocalDateTime timestamp) {
-    public static ApiError of(String code, String message) {
-        return new ApiError(code, message, LocalDateTime.now());
-    }
-}
--- Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/repository/CriterionTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seal.hackathon.repository;
-
-import com.seal.hackathon.entity.CriterionTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CriterionTemplateRepository extends JpaRepository<CriterionTemplate, Integer> {
-}
