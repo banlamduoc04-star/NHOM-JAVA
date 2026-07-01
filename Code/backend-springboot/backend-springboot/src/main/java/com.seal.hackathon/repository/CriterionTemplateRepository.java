dc00630deea900ea1ff23634390052ca1b7e8ef4
MinhThanh <dbuongok32@gmail.com>
2026-07-01 15:20:12 +0700

    Them CriterionTemplateRepository

---
+++ Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/repository/CriterionTemplateRepository.java
@@ -0,0 +1,9 @@
+package com.seal.hackathon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.seal.hackathon.entity.CriterionTemplate;
+
+
+public interface CriterionTemplateRepository extends JpaRepository<CriterionTemplate, Integer> {
+}
