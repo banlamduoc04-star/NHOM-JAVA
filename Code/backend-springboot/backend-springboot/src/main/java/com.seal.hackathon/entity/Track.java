ff01245aef367ae66c43099e85ea2e2a43686a22
le quoc khanh <banlamduoc04@gmail.com>
2026-07-05 22:43:31 +0700

    fix loi file code

--- Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/entity/Track.java
+++ Code/backend-springboot/backend-springboot/src/main/java/com.seal.hackathon/entity/Track.java
@@ -1,11 +1,8 @@
 package com.seal.hackathon.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.*;
 
 @Entity
 @Table(name = "tracks")
