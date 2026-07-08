48b2e174bc4e75fa7a146680ed84c603994b5a45
ThanhTran <dbuongok32@gmail.com>
2026-07-08 18:00:25 +0700

    Fix HackathonEventRepository

--- Code/backend-springboot/src/main/java/com/seal/hackathon/repository/HackathonEventRepository.java
+++ Code/backend-springboot/src/main/java/com/seal/hackathon/repository/HackathonEventRepository.java
@@ -1,8 +1,14 @@
 package com.seal.hackathon.repository;
 
-import com.seal.hackathon.entity.HackathonEvent;
+// Import entity HackathonEvent.
+// Repository này sẽ thao tác với dữ liệu của HackathonEvent.
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.seal.hackathon.entity.HackathonEvent;
 
-public interface HackathonEventRepository extends JpaRepository<HackathonEvent, Integer> {
-}
+
+// Khai báo interface HackathonEventRepository.
+//
+// Interface này dùng để thao tác với bảng hackathon_events
+// thông qua entity HackathonEvent.
+public interface HackathonEventRepository extends JpaRepository<HackathonEvent, Integer> { }
